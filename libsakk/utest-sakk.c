bd4ab23a1715cc61026ce64a533ee917d7ad4fd6
a-bakk <abelbakk@gmail.com>
2022-06-10 17:57:50 +0300

    created unit tests

---
+++ libsakk/utest-sakk.c
@@ -0,0 +1,21 @@
+#include <sakk.h>
+#include <utest-sakk.h>
+#include <CUnit/CUnit.h>
+
+static void nem_tul_okos_teszt() {
+    int a = 1;
+    CU_ASSERT_TRUE(a);
+}
+
+CU_TestInfo sakk_tests[] = {
+    {"teszt teszt",         nem_tul_okos_teszt},
+    CU_TEST_INFO_NULL
+};
+
+void sakk_test_setup() {
+    // letrehozni a ket jatekost
+}
+
+void sakk_test_teardown() {
+    // torolni i guess
+}
---
+++ libsakk/utest.c
@@ -0,0 +1,23 @@
+#include <sakk.h>
+#include <utest-sakk.h>
+#include <utest-sakk.h>
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+
+CU_SuiteInfo sakk_suites[] = {
+    {"sakk", NULL, NULL, sakk_test_setup, sakk_test_teardown, sakk_tests},
+    CU_SUITE_INFO_NULL
+};
+
+int main() {
+    if (CU_initialize_registry() != CUE_SUCCESS) {
+        return -1;
+    };
+    if (CU_register_suites(sakk_suites) != CUE_SUCCESS) {
+        return -1;
+    };
+    CU_basic_set_mode(CU_BRM_NORMAL);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return 0; 
+}
