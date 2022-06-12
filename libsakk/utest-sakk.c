b83384ebe8300c099b0d79863fba099f11ba4946
a-bakk <abelbakk@gmail.com>
2022-06-12 12:51:02 +0300

    cnt of unit tests

--- libsakk/utest-sakk.c
+++ libsakk/utest-sakk.c
@@ -18,8 +18,39 @@ static void test_gyalog_lepes() {
     CU_ASSERT_FALSE(gyalog_lepes(&honnan_s, &honnan_o, &hova_s, &hova_o, &szin)); //elvart: elore nem lephet kettot
 }
 
+static void test_bastya_lepes() {
+    int honnan_s = 0, honnan_o = 0, hova_s = 0, hova_o = 5;
+    CU_ASSERT_TRUE(bastya_lepes(&honnan_s, &honnan_o, &hova_s, &hova_o));
+    hova_s = 1;
+    CU_ASSERT_FALSE(bastya_lepes(&honnan_s, &honnan_o, &hova_s, &hova_o));
+    hova_s = 7; hova_o = 3;
+    CU_ASSERT_FALSE(bastya_lepes(&honnan_s, &honnan_o, &hova_s, &hova_o));
+}
+
+static void test_huszar_lepes() {
+    
+}
+
+static void test_kiraly_lepes() {
+
+}
+
+static void test_futo_lepes(){
+    int honnan_s = 0, honnan_o = 2, hova_s = 3, hova_o = 5;
+
+    CU_ASSERT_TRUE(futo_lepes(&honnan_s, &honnan_o, &hova_s, &hova_o));
+    hova_s = 2;
+    CU_ASSERT_FALSE(bastya_lepes(&honnan_s, &honnan_o, &hova_s, &hova_o));
+    hova_s = 2; hova_o = 0;
+    CU_ASSERT_FALSE(bastya_lepes(&honnan_s, &honnan_o, &hova_s, &hova_o));
+}
+
 CU_TestInfo sakk_tests[] = {
     {"gyalogos lepese",         test_gyalog_lepes},
+    {"bastya lepese",           test_bastya_lepes},
+    {"huszar lepese",           test_huszar_lepes},
+    {"kiraly lepese",           test_kiraly_lepes},
+    {"futo lepese",             test_futo_lepes},
     CU_TEST_INFO_NULL
 };
 
--- libsakk/utest.c
+++ libsakk/utest.c
@@ -16,7 +16,7 @@ int main() {
     if (CU_register_suites(sakk_suites) != CUE_SUCCESS) {
         return -1;
     };
-    CU_basic_set_mode(CU_BRM_NORMAL);
+    CU_basic_set_mode(CU_BRM_VERBOSE); //CU_BRM_NORMAL
     CU_basic_run_tests();
     CU_cleanup_registry();
     return 0; 
